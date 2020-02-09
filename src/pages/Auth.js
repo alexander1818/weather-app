4927b4ca3420bb09f49f7880d12dd3a300efb2a0
alexander1818 <samsungi777s@gmail.com>
2020-02-09 13:23:02 +0200

    added auth page

---
+++ src/pages/Auth.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap';
+
+const Auth = props => {
+
+    return (
+        <Container className="vh-100 ">
+            <Row className="justify-content-center bg-warning vh-100 ">
+                <Col xs="6" className="d-flex align-items-center text-center">
+                    <div className="w-100 text-center"> AUTH FORM</div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Auth
